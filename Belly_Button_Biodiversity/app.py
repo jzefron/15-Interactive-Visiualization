61265c2d1a0128e7899963ead81e8486f8a16094
efron <jonefron@gmail.com>
2019-03-28 17:02:10 -0500

    'actual\sorting'

--- Belly_Button_Biodiversity/app.py
+++ Belly_Button_Biodiversity/app.py
@@ -89,11 +89,17 @@ def samples(sample):
     # Filter the data based on the sample number and
     # only keep rows with values above 1
     sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
+    print(sample_data)
+    print(df[sample])
+    sorted_sample_data = sample_data.sort_values(by = [sample], ascending=False)
+
     # Format the data to send as json
+    
+    print(sorted_sample_data)
     data = {
-        "otu_ids": sample_data.otu_id.values.tolist(),
-        "sample_values": sample_data[sample].values.tolist(),
-        "otu_labels": sample_data.otu_label.tolist(),
+        "otu_ids": sorted_sample_data.otu_id.values.tolist(),
+        "sample_values": sorted_sample_data[sample].values.tolist(),
+        "otu_labels": sorted_sample_data.otu_label.tolist(),
     }
     return jsonify(data)
 
